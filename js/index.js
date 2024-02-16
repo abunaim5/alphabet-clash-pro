69402085e183c2b47e2ee5c3955e410d6dc4a0aa
abunaim5 <abunaim140@gmail.com>
2024-02-16 13:06:36 +0600

    added continue game and game over function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,50 @@
+// keyboard event
+function handleKeyboardEventByKeys(e) {
+    const keyboardKey = e.key;
+    const currentAlphabet = getElementsInnerTextById('current-alphabet');
+    if (keyboardKey === currentAlphabet) {
+        const currentScore = getElementsValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setElementsInnerTextById('current-score', updatedScore);
+        removeElementBgColorById(currentAlphabet);
+        continueGame();
+    }
+    else {
+        const lifeScore = getElementsValueById('life-score');
+        const availableLife = lifeScore - 1;
+        setElementsInnerTextById('life-score', availableLife);
+        if (availableLife === 0) {
+            gameOver();
+        }
+    }
+}
+document.addEventListener('keyup', handleKeyboardEventByKeys);
+
+// home screen
 function playNow() {
     addElementsClassById('home-screen');
-
+    addElementsClassById('score-board');
     removeElementsClassById('playground-screen');
+    continueGame();
+    setElementsInnerTextById('life-score', 5);
+    setElementsInnerTextById('current-score', 0);
+}
 
+// playground screen
+function continueGame() {
+    // generate random index number
     const index = getRandomNumber();
-
     const alphabet = showAlphabetByIndex(index);
-
-    setElementsInnerTextById('show-alphabet', alphabet);
-
+    console.log(alphabet);
+    setElementsInnerTextById('current-alphabet', alphabet);
     setElementBgColorById(alphabet);
+}
+
+// game over screen
+function gameOver() {
+    addElementsClassById('home-screen');
+    addElementsClassById('playground-screen');
+    removeElementsClassById('score-board');
+    const currentAlphabet = getElementsInnerTextById('current-alphabet');
+    removeElementBgColorById(currentAlphabet);
 }
--- js/utility.js
+++ js/utility.js
@@ -3,17 +3,20 @@ function addElementsClassById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
+
 // remove elements Class by elements Id
 function removeElementsClassById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
 // generate random number
 function getRandomNumber() {
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
     return index;
 }
+
 // display alphabet by generated random number as index
 function showAlphabetByIndex(index) {
     const alphabetStr = 'abcdefghijklmnopqrstuvwxyz';
@@ -21,13 +24,38 @@ function showAlphabetByIndex(index) {
     const alphabet = alphabetArr[index];
     return alphabet;
 }
-// set elements inner text by Id
-function setElementsInnerTextById(elementId, alphabet) {
+
+// get element inner text by id
+function getElementsInnerTextById(elementId) {
     const element = document.getElementById(elementId);
-    element.innerText = alphabet;
+    const elementText = element.innerText;
+    const convertedText = elementText.toLowerCase();
+    return convertedText;
 }
-// change elements background color by Id
+
+// set elements inner text by Id
+function setElementsInnerTextById(elementId, text) {
+    const element = document.getElementById(elementId);
+    element.innerText = text;
+}
+
+// get elements value by id
+function getElementsValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    const value = parseInt(elementText);
+    return value;
+}
+
+// set elements background color by Id
 function setElementBgColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+
+// remove elements background color by id
+function removeElementBgColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
