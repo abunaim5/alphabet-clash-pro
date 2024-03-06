09a45bc0718660aa7f6b197d3bfd128f80818f43
abunaim5 <abunaim140@gmail.com>
2024-03-06 23:01:47 +0600

    added game sound effect and blood effect in play screen and fixing some bugs

--- js/index.js
+++ js/index.js
@@ -1,29 +1,56 @@
+// create audio
+const audio = new Audio();
+let isClickedPlayNow = false;
+let isHomeScreen = true;
+
+const playScreen = document.getElementById('play-screen');
 // keyboard event
 function handleKeyboardEventByKeys(e) {
+    handleEscapeAndEnter(e.key);
+    if (!isClickedPlayNow) return;
     const keyboardKey = e.key;
-    if (keyboardKey === 'Escape') {
-        gameOver();
-    }
     const currentAlphabet = getElementsInnerTextById('current-alphabet');
-    if (keyboardKey === currentAlphabet) {
-        const currentScore = getElementsValueById('current-score');
-        const updatedScore = currentScore + 1;
-        setElementsInnerTextById('current-score', updatedScore);
-        removeElementBgColorById(currentAlphabet);
-        continueGame();
+    if (keyboardKey !== "Enter")
+        if (keyboardKey === currentAlphabet) {
+            audio.src = "../audios/short-success-sound.mp3";
+            audio.play();
+
+            const currentScore = getElementsValueById('current-score');
+            const updatedScore = currentScore + 1;
+            setElementsInnerTextById('current-score', updatedScore);
+            removeElementBgColorById(currentAlphabet);
+            continueGame();
+        }
+        else {
+            audio.src = "../audios/failure-drum-sound-effect.mp3";
+            audio.play();
+
+            const lifeScore = getElementsValueById('life-score');
+            const availableLife = lifeScore - 1;
+            let lifePercent = availableLife / 5 * 100;
+            playScreen.style.background = `linear-gradient(#FFFFFFB3 ${lifePercent}%, #880808B3)`;
+            setElementsInnerTextById('life-score', availableLife);
+            if (availableLife === 0) {
+                gameOver();
+            }
+        }
+};
+let isContinueGame = false;
+let isGameOver = true;
+const handleEscapeAndEnter = (keyboardKey) => {
+    if (!isContinueGame) {
+        if (keyboardKey === 'Enter' && isHomeScreen) {
+            isClickedPlayNow = true;
+            playNow();
+        }
     }
-    else {
-        const lifeScore = getElementsValueById('life-score');
-        const availableLife = lifeScore - 1;
-        setElementsInnerTextById('life-score', availableLife);
-        if (availableLife === 0) {
+    if (isGameOver) {
+        if (keyboardKey === 'Escape') {
             gameOver();
         }
     }
-    if (keyboardKey === 'Enter') {
-        playNow();
-    }
-}
+};
+
 document.addEventListener('keyup', handleKeyboardEventByKeys);
 
 // home screen
@@ -34,7 +61,13 @@ function playNow() {
     continueGame();
     setElementsInnerTextById('life-score', 5);
     setElementsInnerTextById('current-score', 0);
-}
+    playScreen.style.background = '';
+    audio.src = "../audios/open-and-closed-door.mp3";
+    audio.play();
+    isClickedPlayNow = true;
+    isContinueGame = true;
+    isGameOver = true;
+};
 
 // playground screen
 function continueGame() {
@@ -43,7 +76,7 @@ function continueGame() {
     const alphabet = showAlphabetByIndex(index);
     setElementsInnerTextById('current-alphabet', alphabet);
     setElementBgColorById(alphabet);
-}
+};
 
 // game over screen
 function gameOver() {
@@ -54,4 +87,9 @@ function gameOver() {
     removeElementBgColorById(currentAlphabet);
     const currentScore = getElementsInnerTextById('current-score');
     setElementsInnerTextById('final-score', currentScore);
-}
+    audio.src = "../audios/game-death-sound.mp3";
+    audio.play();
+    isClickedPlayNow = false;
+    isContinueGame = false;
+    isGameOver = false;
+};
